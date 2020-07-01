a404033324fdd09125ebefc43e74c22c65634a81
Daniel McManus <dan@danmcmanus.dev>
2020-07-01 02:07:45 -0500

    Initial Commit

---
+++ AddOne/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddOne
+//
+//  Created by Daniel McManus on 7/1/20.
+//  Copyright © 2020 Daniel McManus. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
